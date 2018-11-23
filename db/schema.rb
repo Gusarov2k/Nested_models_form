a03c0943769a32c322db3641303e8d79cc330690
gusarov2k <motoface@yandex.ru>
2018-11-23 14:44:55 +0300

    add mehtods

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181123113549) do
+ActiveRecord::Schema.define(version: 20181123113750) do
+
+  create_table "questions", force: :cascade do |t|
+    t.integer  "survey_id"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "surveys", force: :cascade do |t|
     t.string   "name"
